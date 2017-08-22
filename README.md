3b6d1944f64806f5635869d56919e6c6ea966e2a
Jason McKinney <meter_1@yahoo.com>
2017-08-22 17:45:09 -0500

    updated liri.js
    ready for turn-in

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # LIRI
 Node application to display information from Twitter, Spotify, and OMDB
+
+----------
+## Things to implement in future ##
+
+1. Convert to OOP
+2. Consolidate selection functions
+3. Output to logfile
--- liri.js
+++ liri.js
@@ -1,19 +1,27 @@
-var keys = require("./keys.js");
+// LIRI Homework Assignment
+// by Jason Mckinney
+// 8/22/2017
+//
+// Impliments a node.js program to retrieve information for movies, music, or tweets 
+// based on the arguments passed to the program
+//----------------------------------------------------------------------------------
+
+// Module require statements
+var keys = require("./keys.js");	// my API Keys for security
 var fs = require("fs");
 var request = require("request");
 var Twitter = require('twitter');
 
+// Parsing the arguments
+// Pulling Path of the liri.js - avoids filesystem problems
 var leng = Math.max((process.argv[1].lastIndexOf('\\') + 1), (process.argv[1].lastIndexOf('/') + 1))
-var path = process.argv[1].slice(0, leng)
+var path = process.argv[1].slice(0, leng) 
+// Pull the arguments from the process object into my own array disregarding the node.js and liri.js paths
 var args = process.argv;
 args.shift();
 args.shift();
 
-// console.log(path);
-
-
 //main execution
-
 switch (args[0]) {
 	case "do-what-it-says":
 		resolveAction();
@@ -35,20 +43,16 @@ switch (args[0]) {
 		break;
 }
 
-//do-what-it-says - pulls from file random.txt
-
-
+// myTweets function queries the Twitter API on my timeline and returns 20 tweets to the console
+// This will show your last 20 tweets and when they were created at in your terminal/bash window
 function myTweets() {
-	//This will show your last 20 tweets and when they were created at in your terminal/bash window.
-	// request("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=TacochickenNode&count=20", function (error, response, body) {
-	// console.log(keys);
+	// API Call
 	var client = new Twitter(keys.twitter);
 	var params = { screen_name: 'nodejs' };
 	client.get('statuses/home_timeline.json', params, function (error, tweets, response) {
-		// console.log(error);
-		// console.log(body);
+		// if there is NOT an error,
 		if (!error) {
-			//var twweets = JSON.parse(tweets)
+			// Parse the tweets
 			tweets.forEach(function (element) {
 				console.log("By @" + element.user.screen_name);
 				console.log(element.text);
@@ -56,90 +60,60 @@ function myTweets() {
 			}, this);
 		}
 	});
-	// 	console.log(error);
-	// 	console.log(JSON.parse(response));
-	// 	if (!error && response.statusCode === 200) {
-
-	// 		// Then log the body from the site!
-	// 		console.log("body = " + body);
-	// 		var bodyObject = JSON.parse(body);
-	// 		console.log("Changed" + bodyObject);
-	// 		//outputToLog("my-tweets",body);
-	// 	}
-	// });
 }
 
+// spotifyThisSong function takes in a song title as a string, and displays information from the Spotify API
+
 function spotifyThisSong(song) {
-	//Information about a song
-	// This will show the following information about the song in your terminal/bash window
-
-	// Artist(s)
-	// The song's name
-	// A preview link of the song from Spotify
-	// The album that the song is from
-
-	// if no song is provided then your program will default to
-
-	// "The Sign" by Ace of Base
-	// console.log("Entered Spotify");
+	//API Call
 	var Spotify = require('node-spotify-api');
 	var spotify = new Spotify(keys.spotify);
 	spotify.search({ type: 'track', query: song }, function (err, data) {
+		// if there is an error
 		if (err) {
+			// display error
 			return console.log('Error occurred: ' + err);
 		}
-		// console.log(JSON.stringify(data));
+		// otherwise pull the tracks from the results
 		var results = data.tracks.items;
+		// iterate through the results looking for the exact match
 		var i = 0;
 		var found = false;
-		// console.log(typeof results);
-		while (i < results.length && !found) {
-			if (results[i].name.toUpperCase() == song.toUpperCase()) {
+		// loop through the results, and stop either at the end, or if a match is found
+		while (i < results.length && !found) {	
+			// if the name matches
+			if (results[i].name.toUpperCase() == song.toUpperCase()) {	// to find a match, force the case
+				// set found to escape loop
 				found = true;
+				// output!
 				console.log("Artist Name: ", results[i].artists[0].name);
 				console.log("Track Name: ", results[i].name);
 				console.log("Preview: ", results[i].preview_url);
 				console.log("Album: ", results[i].album.name);
 			}
-			else
+			else	// otherwise move to the next element in the array
 				i++;
 		}
+		// if the array is exausted and no match found, 
 		if (!found) {
+			// Sorry!
 			console.log("No song found with that name! Reprhrase?");
 		}
 	});
 }
 
+// movieThis takes a movie title as string and displays information about the movie from OMDB
 function movieThis(movie) {
-	// This will output the following information to your terminal/bash window:
-
-	//   * Title of the movie.
-	//   * Year the movie came out.
-	//   * IMDB Rating of the movie.
-	//   * Country where the movie was produced.
-	//   * Language of the movie.
-	//   * Plot of the movie.
-	//   * Actors in the movie.
-	//   * Rotten Tomatoes URL.
-	// If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
-
-	// If you haven't watched "Mr. Nobody," then you should: http://www.imdb.com/title/tt0485947/
-	// It's on Netflix!
+	// construct URL for API Call
 	var queryUrl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=" + keys.OMDB
 
-	// This line is just to help us debug against the actual URL.
-	//console.log(queryUrl);
-
+	// API Call
 	request(queryUrl, function (error, response, body) {
-
 		// If the request is successful
 		if (!error && response.statusCode === 200) {
-
-			// Parse the body of the site and recover just the imdbRating
-			// (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-			//console.log(body)
+			// pull the title and remove any spaces and special characters for assembly of Rotten Tomatoes URL
 			var urlTitle = JSON.parse(body).Title.toLowerCase().replace(/ /g, "_").replace(/[^\w\s]/gi, '');
-			//console.log(urlTitle)
+			// Display from response
 			console.log("Title: " + JSON.parse(body).Title);
 			console.log("Release Year: " + JSON.parse(body).Year);
 			console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
@@ -152,13 +126,15 @@ function movieThis(movie) {
 			console.log("Rotten Tomatoes Review: https://www.rottentomatoes.com/m/" + urlTitle);
 		}
 	});
-
 }
 
-function outputToLog(command, outputText) {
+// outputToLog is a function would have taken an object and wrote that to an logfile
+function outputToLog(outputObject) {
 	// fs to log.txt
+
 }
 
+// resolveAction is a function that reads the command from random.txt from the path of the liri.js and executes that command
 function resolveAction() {
 	// Reads from Random.txt and determines what to do
 	fs.readFile(path + "random.txt", "utf8", function (error, data) {
@@ -168,35 +144,41 @@ function resolveAction() {
 			return console.log(error);
 		}
 
-		// We will then print the contents of data
-		//   console.log(data);
-		//   console.log(typeof data);
-		// Then split it by commas (to make it more readable)
+		// Parse the data into what would have been the arguement array
 		var dataArr = data.split(",");
-
-		// We will then re-display the content as an array for later use.
-		console.log(dataArr);
-		console.log(typeof dataArr);
-		console.log(dataArr.length);
-
-		 switch (dataArr[0]) {
+		switch (dataArr[0]) {
 			case "my-tweets":
 				myTweets();
 				break;
 			case "spotify-this-song":
 				if (dataArr.length == 2)
-					spotifyThisSong(dataArr[1].replace(/['"]+/g, ''));
+					spotifyThisSong(dataArr[1].replace(/['"]+/g, ''));	// Remove the "s from the string
 				else
 					spotifyThisSong("The Sign");
 				break;
 			case "movie-this":
 				if (dataArr.length == 2)
-					movieThis(dataArr[1]);
+					movieThis(dataArr[1].replace(/['"]+/g, ''));	// Remove the "s from the string
 				else
 					movieThis("Mr. Nobody");
 				break;
 		}
 
 	});
-	// return resultArray
+	// i wanted to return the arguments but due to async issues, decided against it
 }
+
+//KEYFILE SETUP - 
+// module.exports = {
+// 	twitter: {
+// 	  consumer_key: <key>,
+// 	  consumer_secret: <key>,
+// 	  access_token_key: <key>,
+// 	  access_token_secret: <key>
+// 	},
+// 	spotify: {
+// 	  id: <key>,
+// 		  secret: <key>
+// 	},
+// 	OMDB: <key>
+//   }
